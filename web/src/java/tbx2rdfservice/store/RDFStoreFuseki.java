e1bcd56872eb132d6170ef4c9f8794d0233f0897
Victor Rodriguez Doncel <victorr@ac.upc.edu>
2015-07-18 13:28:06 +0200

    .?

--- web/src/java/tbx2rdfservice/store/RDFStoreFuseki.java
+++ web/src/java/tbx2rdfservice/store/RDFStoreFuseki.java
@@ -271,7 +271,7 @@ public class RDFStoreFuseki {
         String sparql = "SELECT DISTINCT ?s\n"
                 + "WHERE {\n"
                 + "  GRAPH ?g {\n"
-                + "    ?s a <http://www.w3.org/2004/02/skos/core#Concept> .\n";
+                + "    ?s a <http://www.w3.org/2004/02/skos/core#Concept> \n";
         if (!searchConcept.isEmpty())
                 sparql+="FILTER regex(?s,'"+searchConcept+"','i') \n";
         sparql += "  }\n"
